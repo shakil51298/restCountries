f2cd5dd23299d17d2093de30554024c5eb259f4a
shakil51298 <zikaiwen02@gmail.com>
2021-02-07 00:43:48 +0800

    80%

--- app.js
+++ app.js
@@ -24,9 +24,45 @@ function shakil(all) {
 function showDetails(name) {
     fetch(`https://restcountries.eu/rest/v2/name/${name}`)
         .then(Response => Response.json())
-        .then(json => console.log(json))
+        .then(json => Details(json))
 
-    // function Details(shakil){
-    // console.log(name);
-    // }
+    function Details(country){
+        const FlagImg = document.getElementById('flagImg');
+        FlagImg.src = country[0].flag;
+
+        const CnameD = document.getElementById('showingName')
+        CnameD.innerText = 'Country Name: '+country[0].name;
+
+        const CnumericCode = document.getElementById('showingNumericCode');
+        CnumericCode.innerText = 'Numeric Code: ' + country[0].numericCode;
+
+        const Cborders = document.getElementById('showingBorders');
+        Cborders.innerText = 'Borders: ' + country[0].borders;
+
+        const Cpopulation = document.getElementById('showingPopilation');
+        Cpopulation.innerText = 'Population: ' + country[0].population;
+
+        const Cregion = document.getElementById('showingRegion');
+        Cregion.innerText = 'Region: ' + country[0].region;
+
+        const CsubRegion = document.getElementById('showingsubRegion');
+        CsubRegion.innerText = 'Sub-Region: ' + country[0].subregion;
+
+        const CtimeZons = document.getElementById('showingTimeZons');
+        CtimeZons.innerText = 'Time-Zones: ' + country[0].timezones[0];
+
+        const CcallingCode = document.getElementById('showingCallingCode');
+        CcallingCode.innerText = 'Calling Code : ' + country[0].callingCodes[0];
+
+        const CcallingArea = document.getElementById('showingArea');
+        CcallingArea.innerText = 'Area : ' + country[0].area;
+
+        const Ccapital = document.getElementById('showingCapital');
+        Ccapital.innerText = 'Capital : ' + country[0].capital;
+
+        const Clanguage = document.getElementById('showinglang');
+        Clanguage.innerText = 'Language : ' + country[0].languages[0].nativeName;
+
+    console.log(country);
+    }
 }
