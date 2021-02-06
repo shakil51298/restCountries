a67f02556d5e0ab96933d2171ce0d30306653a07
shakil51298 <zikaiwen02@gmail.com>
2021-02-06 23:24:36 +0800

    50%

--- app.js
+++ app.js
@@ -3,34 +3,30 @@ fetch('https://restcountries.eu/rest/v2/all')
     .then(data => shakil(data))
 
 function shakil(all) {
-    const mainDiv =document.getElementById('main');
+    const mainDiv = document.getElementById('main');
     for (let i = 0; i < all.length; i++) {
         const country = all[i];
 
         const items = document.createElement('div');
-        items.className='infoDiv'
+        items.className = 'infoDiv'
 
         const countryInfo = `
             <h3 class="countryName">${country.name}</h3>
             <p class="capital">${country.capital}</p>
-            <button onclick="showDetails()" class="btn btn-success">Details</button>
+            <button onclick="showDetails('${country.name}')" class="btn btn-success">Details</button>
         `
         items.innerHTML = countryInfo;
         mainDiv.appendChild(items);
         // console.log(country.area);
     }
-    
 }
 
-function showDetails(){
-    fetch('https://restcountries.eu/rest/v2/name/{country}')
-    .then(Response => Response.json())
-    .then(json => Details(json))
+function showDetails(name) {
+    fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+        .then(Response => Response.json())
+        .then(json => console.log(json))
 
-    function Details(){
-
-    }
-    
-    
-    
+    // function Details(shakil){
+    // console.log(name);
+    // }
 }
